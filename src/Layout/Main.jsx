068cf2abce41095c96cceae4445b19f69d9b666c
Shamimreza82 <shamimrezaone@gmail.com>
2024-08-13 13:04:59 +0600

    added slider

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from '../pages/Shared/Footer/Footer';
 import Navber from '../pages/Shared/Navber/Navber';
+import Banner from '../pages/Home/Banner/Banner';
+import Category from '../pages/Home/Category/Category';
 
 const Main = () => {
     return (
         <div>
             <Navber></Navber>
+            <Banner></Banner>
+            <Category></Category>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
--- src/Router/Routes.jsx
+++ src/Router/Routes.jsx
@@ -1,20 +1,21 @@
 
 import {
-    createBrowserRouter,
-  } from "react-router-dom";
+  createBrowserRouter,
+} from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../pages/Home/Home/Home";
 
-  export const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Main></Main>,
-      children: [
-        {
-            path: '/', 
-            element: <Home></Home>
-        }
-      ]
-    },
-  ]);
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      }
+    ]
+  },
+]);
 
